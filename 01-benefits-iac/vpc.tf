36c6677b269c4b2b5db7728d8e9cac4eaa1f4b72
Rahul Yadav <rahulkyadav9691@gmail.com>
2024-11-05 19:06:52 +0000

    test commit

---
+++ 01-benefits-iac/vpc.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet "demo_public_subnet" {
+    vpc_id = aws_vpc.demo_vpc
+    cidr_block="10.0.0.0/24"
+
+}
+
+resource "aws_subnet "demo_private_subnet" {
+    vpc_id = aws_vpc.demo_vpc
+    cidr_block="10.0.1.0/24"
+}
