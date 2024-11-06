40fd7f939535546b941a2815bbe5b94faf26f1d5
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2024-11-06 23:26:53 +0530

    Update vpc.tf

--- 01-benefits-iac/vpc.tf
+++ 01-benefits-iac/vpc.tf
@@ -7,17 +7,20 @@ terraform {
   }
 }
 
+provider "aws" {
+     region = "ap-south-1"
+}
 resource "aws_vpc" "demo_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet "demo_public_subnet" {
+resource "aws_subnet" "demo_public_subnet" {
     vpc_id = aws_vpc.demo_vpc
     cidr_block="10.0.0.0/24"
 
 }
 
-resource "aws_subnet "demo_private_subnet" {
+resource "aws_subnet" "demo_private_subnet" {
     vpc_id = aws_vpc.demo_vpc
     cidr_block="10.0.1.0/24"
 }
