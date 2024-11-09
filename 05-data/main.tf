fefa739d3b9d54cacf7f1d6b3e7a209e6cf32f2d
Rahul Yadav <rahulkyadav9691@gmail.com>
2024-11-09 07:02:29 +0000

    test changes

---
+++ 05-data/main.tf
@@ -0,0 +1,33 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {
+    provider = aws.second
+}
+
+output "aws_caller_identity" {
+  value = data.aws_caller_identity.current
+}
+
+output "aws_region" {
+    value=data.aws_region.current
+}
+
+# we can also fetch resources based on tags 
+
+data "aws_vpc" "my_vpc" {
+    tags = {
+        Name = "Default VPC"
+    }
+}
+
+output "vpc_data" {
+    value=data.aws_vpc.my_vpc.id
+}
+
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+output "azs" {
+    value=data.aws_availability_zones.available[*].id
+}
