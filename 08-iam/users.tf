cd3080b596472b840f41c452c9011a31cfb10d83
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2024-11-10 12:05:45 +0530

    Update users.tf

--- 08-iam/users.tf
+++ 08-iam/users.tf
@@ -2,6 +2,28 @@ locals {
    users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
 }
 
+resource "aws_iam_user" "users"{
+     for_each = toset(local.users_from_yaml[*].username)
+     name = each.value
+}
+
+
 output "users" {
       value = local.users_from_yaml
 }
+
+
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
